358572fe3fe456eed1da635fc16a01009a1e25de
Yunjian Yang <happyhackingyy@gmail.com>
2017-12-20 10:38:13 -0500

    update

--- init.el
+++ init.el
@@ -12,34 +12,6 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar myPackages
-  '(better-defaults
-    elpy ;; add the elpy package
-    material-theme))
-
-(mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
-      myPackages)
-
-;; BASIC CUSTOMIZATION
-;; --------------------------------------
-
 (setq inhibit-startup-message t) ;; hide the startup message
 (load-theme 'material t) ;; load material theme
-(global-linum-mode t) ;; enable line numbers globally
 (setq column-number-mode t) ;; enable columns
-(elpy-enable) ;;elpy-enable
-;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (material-theme elpy better-defaults))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
